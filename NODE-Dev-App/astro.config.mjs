f2f0bb36865544410dac3fd84bcb24772b2f9eb1
Chad Dupuis <chad@yyhmsg.net>
2025-01-02 15:33:05 -0600

    initial deploy

--- NODE-Dev-App/astro.config.mjs
+++ NODE-Dev-App/astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chattanooga-yinyanghouse.github.io',
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   })]

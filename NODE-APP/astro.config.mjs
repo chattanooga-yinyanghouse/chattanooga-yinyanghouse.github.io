744c5e2019118ca44c30b8a406ec87a13658a40c
Chad Dupuis <chad@yyhmsg.net>
2025-06-16 22:07:18 +0000

    update url

--- NODE-APP/astro.config.mjs
+++ NODE-APP/astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://chattanooga.yinyanghouse.com',
+    site: 'https://yyhchattanooga.com',
     vite: {
         plugins: [tailwindcss()],
     },
